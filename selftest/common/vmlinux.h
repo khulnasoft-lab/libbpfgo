90ec07dfd392aa82c0bde80219f28da4119bea11
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2025-03-19 10:19:47 +0600

    Update selftest/common/vmlinux.h
    Co-authored-by: qodo-merge-pro-for-open-source[bot] <189517486+qodo-merge-pro-for-open-source[bot]@users.noreply.github.com>
    Signed-off-by: gitworkflows <118260833+gitworkflows@users.noreply.github.com>

--- selftest/common/vmlinux.h
+++ selftest/common/vmlinux.h
@@ -4,9 +4,9 @@
 ; // don't remove: clangd parsing bug https://github.com/clangd/clangd/issues/1167
 #pragma clang attribute push(__attribute__((preserve_access_index)), apply_to = record)
 
-enum {
-    false = 0,
-    true = 1,
+enum bool_values {
+    false_value = 0,
+    true_value = 1,
 };
 
 typedef signed char __s8;
